538922ffac5be24ac6571ad6026f49378048a1f9
xowjd1 <taejeong311@gmail.com>
2025-07-15 17:50:08 +0900

    술병 던지기 패턴

---
+++ Assets/KTJ/Scripts/Enemy/NormalOni.cs
@@ -0,0 +1,155 @@
+using UnityEngine;
+
+//플레이어 조우 전 춤추는 상태
+public class OniDanceState : OniState
+{
+    public OniDanceState(OniStateMachine oniStateMachine) : base(oniStateMachine)
+    {
+        
+    }
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+    
+}
+
+// 플레이어 조우 상태
+public class OniIntroState : OniState
+{
+    private GameObject _player;
+    public OniIntroState(OniStateMachine oniStateMachine, GameObject player) : base(oniStateMachine)
+    {
+        _player = player;
+    }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
+
+// 술병 던지기 패턴
+public class OniThrowJarState : OniState
+{
+    private GameObject _player;
+    private Transform _jarThrowPoint;
+    private GameObject _jar;
+    private float throwForce = 10f;
+    private bool hasThrown = false;
+    
+    public OniThrowJarState(OniStateMachine oniStateMachine, GameObject player,
+        Transform jarThrowPoint, GameObject jar) : base(oniStateMachine)
+    {
+        _player = player;
+        _jarThrowPoint = jarThrowPoint;
+        _jar = jar;
+    }
+
+    public override void Enter()
+    {
+        hasThrown = false;
+    }
+
+    public override void Update()
+    {
+        if (hasThrown)
+            return;
+
+        ThrowJar();
+        hasThrown = true;
+
+    }
+
+    private void ThrowJar()
+    {
+        if(_jar == null || _jarThrowPoint == null)
+            return;
+
+        _jar.transform.SetParent(null);
+        
+        Rigidbody rb = _jar.GetComponent<Rigidbody>();
+        if(rb == null)
+            rb = _jar.AddComponent<Rigidbody>();
+        
+        rb.isKinematic = false;
+        rb.useGravity = true;
+
+        Vector3 dir = (_player.transform.position - _jarThrowPoint.position).normalized;
+        rb.linearVelocity = dir * throwForce + Vector3.up * 2f;
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
+
+// 플레이어한테 뛰어가서 주먹공격 패턴
+public class OniPunchState : OniState
+{
+    private GameObject _player;
+    public OniPunchState(OniStateMachine oniStateMachine, GameObject player) : base(oniStateMachine)
+    {
+        _player = player;
+    }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
+
+// 죽음 상태
+public class OniDieState : OniState
+{
+    public OniDieState(OniStateMachine oniStateMachine) : base(oniStateMachine)
+    {
+        
+    }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
--- Assets/KTJ/Scripts/Enemy/OniStateMachine.cs
+++ Assets/KTJ/Scripts/Enemy/OniStateMachine.cs
@@ -3,6 +3,73 @@ using UnityEngine;
 public class OniStateMachine : MonoBehaviour
 {
     private OniState currentState;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform jarThrowPoint;
+    [SerializeField] private GameObject jar;
     
+
+    private void Awake()
+    {
+        
+        
+    }
+
+    private void Start()
+    {
+        ChangeState(CreateOniThrowJarState());
+    }
+
+    private void Update()
+    {
+        currentState?.Update();
+    }
+
+    public void ChangeState(OniState newState)
+    {
+        currentState?.Exit();
+        currentState = newState;
+        currentState?.Enter();
+    }
+    
+    public OniState CreateOniDanceState()
+    {
+        return new OniDanceState(
+            this
+            
+        );
+    }
+
+    public OniState CreateOniIntroState()
+    {
+        return new OniIntroState(
+            this,
+            player
+        );
+    }
+
+    public OniState CreateOniThrowJarState()
+    {
+        return new OniThrowJarState(
+            this,
+            player,
+            jarThrowPoint,
+            jar
+        );
+    }
+
+    public OniState CreateOniPunchState()
+    {
+        return new OniPunchState(
+            this,
+            player
+        );
+    }
+
+    public OniState CreateOniDieState()
+    {
+        return new OniDieState(
+            this
+        );
+    }
     
 }
